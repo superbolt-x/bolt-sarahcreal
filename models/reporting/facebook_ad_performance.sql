0cb42b23a326c871d916505f54cbea4c99800398
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-01-31 16:28:10 -0500

    fix

--- models/reporting/facebook_ad_performance.sql
+++ models/reporting/facebook_ad_performance.sql
@@ -3,8 +3,8 @@
 )}}
 
 SELECT
-    CASE WHEN account_id = '816379750560368' THEN "DTC"
-         WHEN account_id = '1697010251141731' THEN "Sephora"
+    CASE WHEN account_id = '816379750560368' THEN 'DTC'
+         WHEN account_id = '1697010251141731' THEN 'Sephora'
     END AS account,
 campaign_name,
 campaign_id,
