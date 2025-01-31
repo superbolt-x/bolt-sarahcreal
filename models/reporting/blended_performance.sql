12406c8464cfb2e2f287be086a7b7bfd1490f94b
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-01-31 16:04:15 -0600

    Update blended_performance.sql
    paid_

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -4,15 +4,15 @@
 
 WITH paid_data as
     (SELECT channel, date::date, date_granularity, COALESCE(SUM(spend),0) as spend, COALESCE(SUM(clicks),0) as clicks, COALESCE(SUM(impressions),0) as impressions, 
-        COALESCE(SUM(purchases),0) as purchases, COALESCE(SUM(revenue),0) as revenue
+        COALESCE(SUM(paid_purchases),0) as paid_purchases, COALESCE(SUM(paid_revenue),0) as paid_revenue
     FROM
         (SELECT 'Meta' as channel, date, date_granularity, 
-            spend, link_clicks as clicks, impressions, purchases, revenue
+            spend, link_clicks as clicks, impressions, purchases as paid_purchases, revenue as paid_revenue
         FROM {{ source('reporting','facebook_ad_performance') }}
         WHERE account = 'DTC'
         UNION ALL
         SELECT 'Google Ads' as channel, date, date_granularity,
-            spend, clicks, impressions, purchases, revenue
+            spend, clicks, impressions, purchases as paid_purchases, revenue as paid_revenue
         FROM {{ source('reporting','googleads_campaign_performance') }}
         )
     GROUP BY channel, date, date_granularity)
@@ -23,6 +23,6 @@ SELECT channel,
     spend,
     clicks,
     impressions,
-    purchases,
-    revenue
+    paid_purchases,
+    paid_revenue
 FROM paid_data
