66a0d10bf8291c11ec939dcc8378c23699052dbc
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-01-31 15:01:03 -0500

    debut

--- models/reporting/facebook_ad_performance.sql
+++ models/reporting/facebook_ad_performance.sql
@@ -5,7 +5,7 @@
 SELECT
     CASE WHEN account_id="816379750560368" THEN "DTC"
          WHEN account_id="1697010251141731" THEN "Sephora"
-    END AS account
+    END AS account,
 campaign_name,
 campaign_id,
 campaign_effective_status,
