d96d3a2d7df2949138e7aa0f9a3ccc7680bebe5b
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-02-20 11:52:58 -0500

    Update blended_performance.sql

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -4,7 +4,7 @@
 
 WITH paid_data as
     (SELECT channel, date::date, date_granularity, COALESCE(SUM(spend),0) as spend, COALESCE(SUM(clicks),0) as clicks, COALESCE(SUM(impressions),0) as impressions, 
-        COALESCE(SUM(paid_purchases),0) as paid_purchases, COALESCE(SUM(paid_revenue),0) as paid_revenue
+        COALESCE(SUM(paid_purchases),0) as paid_purchases, COALESCE(SUM(paid_revenue),0) as paid_revenue, 0 as shopify_total_sales, 0 as shopify_orders
     FROM
         (SELECT 'Meta' as channel, date, date_granularity, 
             spend, link_clicks as clicks, impressions, purchases as paid_purchases, revenue as paid_revenue
@@ -15,7 +15,13 @@ WITH paid_data as
             spend, clicks, impressions, purchases as paid_purchases, revenue as paid_revenue
         FROM {{ source('reporting','googleads_campaign_performance') }}
         )
-    GROUP BY channel, date, date_granularity)
+    GROUP BY channel, date, date_granularity),
+
+sho_data as
+    (
+        SELECT total_sales as shopify_total_sales, orders as shopify_orders
+        FROM {{ source('reporting','shopify_sales') }}
+    )
     
 SELECT channel,
     date,
@@ -24,5 +30,9 @@ SELECT channel,
     clicks,
     impressions,
     paid_purchases,
-    paid_revenue
-FROM paid_data
+    paid_revenue,
+    shopify_total_sales,
+    shopify_orders
+FROM (
+    SELECT * FROM paid_data UNION ALL sho_data
+)
