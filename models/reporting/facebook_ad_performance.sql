9953c2bf2a83d6cbd63549d4282cb3dcddc6fce1
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-02-28 18:55:17 +0100

    Update facebook_ad_performance.sql

--- models/reporting/facebook_ad_performance.sql
+++ models/reporting/facebook_ad_performance.sql
@@ -28,5 +28,7 @@ impressions,
 link_clicks,
 add_to_cart,
 purchases,
-revenue
+revenue,
+omni_purchase_with_shared_items as purchases_shared_items,
+omni_purchase_with_shared_items_value as revenue_shared_items
 FROM {{ ref('facebook_performance_by_ad') }}
