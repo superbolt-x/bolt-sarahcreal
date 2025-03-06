f88e2571b91d7a1d608ce75972efe808ad68ca1a
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-06 19:59:54 +0100

    Create facebook_campaign_performance.sql

---
+++ models/reporting/facebook_campaign_performance.sql
@@ -0,0 +1,23 @@
+{{ config (
+    alias = target.database + '_facebook_campaign_performance'
+)}}
+
+SELECT
+    CASE WHEN account_id = '816379750560368' THEN 'DTC'
+         WHEN account_id = '1697010251141731' THEN 'Sephora'
+    END AS account,
+campaign_name,
+campaign_id,
+campaign_effective_status,
+campaign_type_default,
+date,
+date_granularity,
+spend,
+impressions,
+link_clicks,
+add_to_cart,
+purchases,
+revenue,
+omni_purchase_with_shared_items as purchases_shared_items,
+omni_purchase_with_shared_items_value as revenue_shared_items
+FROM {{ ref('facebook_performance_by_campaign') }}
