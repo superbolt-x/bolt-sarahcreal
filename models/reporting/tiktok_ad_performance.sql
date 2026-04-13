cc4124b46528177e03ebdc31ee1668fb07efcd25
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-04-13 10:35:21 +0200

    Update tiktok_ad_performance.sql

--- models/reporting/tiktok_ad_performance.sql
+++ models/reporting/tiktok_ad_performance.sql
@@ -20,7 +20,6 @@ date_granularity,
 cost as spend,
 impressions,
 clicks,
-complete_payment_events as purchases,
-complete_payment_value as revenue,
-web_add_to_cart_events as atc
+complete_payment as purchases,
+complete_payment_rate as revenue
 FROM {{ ref('tiktok_performance_by_ad') }}
